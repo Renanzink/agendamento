3c25f207c0854723ae01f9d1507643292e4f40ac
renan <renankill10@gmail.com>
2026-01-13 18:02:33 -0300

    login emy

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
-SELECT * FROM usuarios;
+INSERT INTO usuarios (username, password, enabled)
+VALUES ('emylle', '$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xdqD1RPHu5vGhzS6', true)
+ON CONFLICT (username) DO NOTHING;
