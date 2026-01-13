f3a40e24e848cb4e7d8074a86cb3372695e5188f
renan <renankill10@gmail.com>
2026-01-13 18:23:29 -0300

    Att properties and data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,2 @@
-INSERT INTO usuarios (username, password, enabled)
-VALUES ('emylle', '$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xdqD1RPHu5vGhzS6', true)
-ON CONFLICT (username) DO NOTHING;
+DELETE FROM usuarios WHERE login = 'emylle';
+INSERT INTO usuarios (login, senha) VALUES ('emylle', '$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xdqD1RPHu5vGhzS6');
