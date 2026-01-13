b00e1a6fda1df36be22f064bda25767f91506b7e
renan <renankill10@gmail.com>
2026-01-13 18:57:13 -0300

    att new.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
 DELETE FROM usuarios WHERE login = 'emylle';
-INSERT INTO usuarios (login, senha) VALUES ('emylle', '$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xdqD1RPHu5vGhzS6');
+INSERT INTO usuarios (login, senha) VALUES ('emylle', 'senha123');
