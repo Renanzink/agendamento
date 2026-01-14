9673944988332179a03440d04d8e4c3f973ad96c
renan <renankill10@gmail.com>
2026-01-14 19:27:23 -0300

    update 3.2

--- src/main/java/com/arquiteta/agendamento/config/UserDetailsServiceImpl.java
+++ src/main/java/com/arquiteta/agendamento/config/UserDetailsServiceImpl.java
@@ -17,14 +17,24 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // --- TESTE DE EMERGÊNCIA ---
+        if ("emylle".equals(username)) {
+            System.out.println("DEBUG LOGIN -> Usando usuário virtual de emergência");
+            return User.withUsername("emylle")
+                    .password("senha123")
+                    .authorities("ROLE_ADMIN")
+                    .build();
+        }
+        // ---------------------------
+
         Usuario usuario = repository.findByLogin(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
 
-        System.out.println("DEBUG LOGIN -> Usuário: " + usuario.getLogin() + " | Senha: " + usuario.getSenha());
+        System.out.println("DEBUG LOGIN -> Usuário do Banco: " + usuario.getLogin());
 
         return User.withUsername(usuario.getLogin())
                 .password(usuario.getSenha())
-                .authorities("ROLE_ADMIN") // Define que ele é administrador
+                .authorities("ROLE_ADMIN")
                 .build();
     }
 }
